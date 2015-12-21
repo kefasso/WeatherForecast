a30d186ebc6bad38fd6837a1e3912fd0fc3a1462
Kamil Kulinski <kamil.kulinski@oke.pl>
2015-12-21 18:28:11 +0100

    * new custom cells

---
+++ WeatherForecast/WeatherForecast/WeatherForecast/WeatherCell.h
@@ -0,0 +1,13 @@
+//
+//  WeatherCell.h
+//  WeatherForecast
+//
+//  Created by Kefas on 21.12.2015.
+//  Copyright © 2015 QL-Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherCell : UITableViewCell
+
+@end
---
+++ WeatherForecast/WeatherForecast/WeatherForecast/WeatherCell.m
@@ -0,0 +1,23 @@
+//
+//  WeatherCell.m
+//  WeatherForecast
+//
+//  Created by Kefas on 21.12.2015.
+//  Copyright © 2015 QL-Solutions. All rights reserved.
+//
+
+#import "WeatherCell.h"
+
+@implementation WeatherCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
